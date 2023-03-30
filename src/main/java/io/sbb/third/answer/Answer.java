15663cce88b7f0abc8af6a08bf7493b1ba649a34
eoeo2255 <naiga6@gmail.com>
2023-03-30 15:49:25 +0900

    세팅과 DB연결 í

---
+++ src/main/java/io/sbb/third/answer/Answer.java
@@ -0,0 +1,21 @@
+package io.sbb.third.answer;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 200)
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime createDate;
+}
---
+++ src/test/java/io/sbb/third/ThirdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sbb.third;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
