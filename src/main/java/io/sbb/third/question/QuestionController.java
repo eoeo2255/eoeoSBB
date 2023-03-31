c7a5bad7415025f569411ffe3f0c12cc69607f53
eoeo2255 <naiga6@gmail.com>
2023-03-31 13:34:08 +0900

    2-10 질문 상세

--- src/main/java/io/sbb/third/question/QuestionController.java
+++ src/main/java/io/sbb/third/question/QuestionController.java
@@ -4,18 +4,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService; //서비스가 생겼기 때문에 컨트롤러는 서비스를 통해 DB에 접근할 수 있다.
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
-        List<Question> lq = this.questionService.getQ();
+        List<Question> lq = this.questionService.getQList();
         model.addAttribute("questionList", lq); // 모델 객체에 값을 담아두면 템플릿에서 사용할 수 있다.
 
         return "question_list";
@@ -25,6 +28,15 @@ public class QuestionController {
     public String root() {
         return "redirect:/question/list";
     }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question q = this.questionService.getQ(id);
+        model.addAttribute("question",q);
+        return "question_detail";
+    }
+
+
 
 
 
--- src/main/java/io/sbb/third/question/QuestionService.java
+++ src/main/java/io/sbb/third/question/QuestionService.java
@@ -1,17 +1,34 @@
 package io.sbb.third.question;
 
+import io.sbb.third.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getQ() {
+    public List<Question> getQList() {
         return this.questionRepository.findAll(); //컨트롤러에서 바로 받지 않고 서비스에서 받아가야 하기 때문에 서비스에서 로직을 짠다.
     }
+
+    public Question getQ(Integer id) {
+        Optional<Question> oq = this.questionRepository.findById(id);
+
+        if (oq.isPresent()) {
+            return oq.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
+
+
 
 }
